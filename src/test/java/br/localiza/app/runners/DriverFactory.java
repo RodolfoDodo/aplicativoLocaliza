a2f8f3c3f4569f8f71dad8685d54678e300a4946
Rodolfo Lacerda - BHS <bhs66@localiza.com>
2020-11-30 12:06:33 -0300

    first commit

--- src/test/java/br/localiza/app/runners/DriverFactory.java
+++ src/test/java/br/localiza/app/runners/DriverFactory.java
@@ -34,8 +34,8 @@ public class DriverFactory {
 		desiredCapabilities.setCapability("platformName", "Android");
 		desiredCapabilities.setCapability("appPackage", "com.localiza.combustivelapp");
 		desiredCapabilities.setCapability("appActivity", "com.localiza.combustivelapp.ui.main.MainActivity");
-		desiredCapabilities.setCapability("deviceName", "Nexus 6 API 29");
-		desiredCapabilities.setCapability("platformVersion", "7");
+		desiredCapabilities.setCapability("deviceName", "Nexus S");
+		desiredCapabilities.setCapability("platformVersion", "10");
 		desiredCapabilities.setCapability("autoGrantPermissions", true);
 		desiredCapabilities.setCapability("ensureWebviewsHavePages", true);
 		
@@ -62,8 +62,10 @@ public class DriverFactory {
 		desiredCapabilities.setCapability("platformName", "Android");
 		desiredCapabilities.setCapability("appPackage", "com.localiza.menuapp");
 		desiredCapabilities.setCapability("appActivity", "com.localiza.menuapp.ui.main.MainActivity");
-		desiredCapabilities.setCapability("deviceName", "Nexus 6 API 29");
-		desiredCapabilities.setCapability("platformVersion", "7");
+		desiredCapabilities.setCapability("deviceName", "Nexus S");
+		desiredCapabilities.setCapability("platformVersion", "10");
+		//desiredCapabilities.setCapability("deviceName", "Nexus 6 API 29");
+		//desiredCapabilities.setCapability("platformVersion", "7");
 		desiredCapabilities.setCapability("autoGrantPermissions", true);
 		desiredCapabilities.setCapability("ensureWebviewsHavePages", true);
 		
